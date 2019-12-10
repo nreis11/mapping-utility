f0b82aadf3f682778c5ffaa8688b6346889dff59
Nicholas <nick.reis@equest.com>
2019-12-10 15:55:40 -0800

    Update tests

--- src/components/HeaderContainer/BoardNameForm.test.js
+++ src/components/HeaderContainer/BoardNameForm.test.js
@@ -5,32 +5,29 @@ import { Form } from "react-bootstrap";
 
 describe("<BoardNameForm/>", () => {
   let wrapper;
-  let boardName = "Board";
+  const props = {
+    boardName: "Board",
+    handleInputChange: jest.fn()
+  };
 
   it("renders the form", () => {
-    wrapper = shallow(
-      <BoardNameForm handleInputChange={() => {}} name={boardName} />
-    );
+    wrapper = shallow(<BoardNameForm {...props} />);
     const form = wrapper.find(Form);
     expect(form.length).toBe(1);
   });
 
-  it("Renders changes", () => {
+  it("Handles changes", () => {
     const newBoardName = "NEW";
-    wrapper = mount(
-      <BoardNameForm handleInputChange={() => {}} name={boardName} />
-    );
+    wrapper = mount(<BoardNameForm {...props} />);
     const stateKey = wrapper.find("#formBoardName").prop("name");
     wrapper.find("#formBoardName").simulate("change", {
       target: { name: stateKey, value: newBoardName }
     });
-    expect(wrapper.find("#formBoardName").prop("value")).toEqual(newBoardName);
+    expect(wrapper.prop("handleInputChange")).toHaveBeenCalled();
   });
 
   it("allows editing when edit button is clicked", () => {
-    wrapper = mount(
-      <BoardNameForm handleInputChange={() => {}} name={boardName} />
-    );
+    wrapper = mount(<BoardNameForm {...props} />);
     expect(wrapper.find("#formBoardName").prop("disabled")).toBe(true);
     // HandleEdit button
     wrapper.find(".btn").simulate("click");
--- src/components/HeaderContainer/TypeSelector.test.js
+++ src/components/HeaderContainer/TypeSelector.test.js
@@ -1,26 +1,35 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 import TypeSelector from "./TypeSelector";
+import { Dropdown } from "react-bootstrap";
 import { TYPES } from "../../utils/mappingHelpers";
 
 const activeType = Object.keys(TYPES)[0];
-const handleSelect = () => {};
+const handleSelect = jest.fn();
 
 describe("<TypeSelector/>", () => {
   let wrapper;
 
-  it("renders without crashing", () => {
-    wrapper = mount(
+  beforeEach(() => {
+    wrapper = shallow(
       <TypeSelector activeType={activeType} onSelect={handleSelect} />
     );
   });
 
   it("Renders the active type label", () => {
-    wrapper = shallow(
-      <TypeSelector activeType={activeType} onSelect={handleSelect} />
-    );
+    const dropdownBtn = wrapper.find("#dropdown-basic-type");
+    expect(dropdownBtn.text()).toEqual(TYPES[activeType]);
+  });
 
-    const dropdownBtn = wrapper.find("DropdownButton");
-    expect(dropdownBtn.prop("title")).toBe(TYPES[activeType]);
+  it("Renders all menu items", () => {
+    const dropDownItems = wrapper.find(Dropdown.Item);
+    expect(dropDownItems.length).toEqual(Object.keys(TYPES).length);
+  });
+
+  it("Calls proper event handler on select", () => {
+    const firstChild = wrapper.find(Dropdown.Item).at(0);
+    console.log("firstChild: ", firstChild.debug());
+    firstChild.simulate("select");
+    expect(firstChild.prop("onSelect")).toHaveBeenCalled();
   });
 });
